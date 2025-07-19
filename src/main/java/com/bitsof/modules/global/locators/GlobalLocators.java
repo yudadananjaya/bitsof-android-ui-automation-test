b08b364d6d8dd94da6d3fbcb699b10034c8bc5c7
unknown <yudananjayaii@gmail.com>
2025-07-20 02:20:02 +0700

    clean up code

--- src/main/java/com/bitsof/modules/global/locators/GlobalLocators.java
+++ src/main/java/com/bitsof/modules/global/locators/GlobalLocators.java
@@ -1,10 +1,6 @@
 package com.bitsof.modules.global.locators;
 
 public class GlobalLocators {
-    // Update these locators to match Android UI if possible (resource-id,
-    // content-desc, etc.)
-    public static final String online_status_text = "//*[@content-desc='online_status_text']";
-
     public static String getUsernameLocator(String username) {
         // Example: return String.format("//*[@text='%s']", username);
         return String.format("//*[@text='%s']", username);
